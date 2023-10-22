ffb12c22a3f8b209a0e8461711895dfc0ac4311f
Gustavo Hackime Costa <gustavohackime@gmail.com>
2023-10-22 03:17:58 -0300

    Absolute path to json file

--- javascript/modal.js
+++ javascript/modal.js
@@ -7,7 +7,7 @@ const openingBtns = document.getElementsByClassName("btn-question-gen");
 const closingBtn = document.getElementById("closeModalBtn");
 
 let configsJsonFile;
- fetch('../json/questions.json')
+ fetch('https://iamindex.github.io/json/questions.json')
     .then( (response)=> {return response.json()} )
     .then(json=> configsJsonFile=json);
 
